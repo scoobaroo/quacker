0d58c073803c92666afe6ccc89873c8f34ae83ac
jabad28 <abad.joanne@yahoo.com>
2016-05-12 16:31:36 -0700

    saving

--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20160512193343) do
-  create_table "users", force: :cascade do |t|
-    t.string   "username"
-    t.string   "email"
-    t.string   "password_digest"
-    t.string   "current_city"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
-end
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "tweets", force: :cascade do |t|
     t.string   "title"
@@ -30,4 +24,14 @@ end
     t.float    "latitude"
     t.float    "longitude"
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
+    t.string   "password_digest"
+    t.string   "current_city"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
 end
