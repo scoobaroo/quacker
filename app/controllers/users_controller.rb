8c5f96cc0f8939015c16e75b7c1c4193a0b9d449
ilterates <iltates@gmail.com>
2016-05-12 12:34:51 -0700

    created user routes, created index and create method in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,25 @@
 class UsersController < ApplicationController
 
   def index
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      login(@user)
+      redirect to @user
+    else
+      render :index
+    end
+  end
+
+
+  private
+
+  def user_params
+
+    params.require(:user).permit(:username, :email,:password, :current_city)
 
   end
 end
