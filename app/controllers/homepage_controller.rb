ad19702a7495140a621accc36e98ed955f9f3fd5
Anna Sobolewska <sobolewska.anna@gmail.com>
2016-05-14 20:17:05 -0700

    fixed route for homepage and upated controller

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,2 +1,6 @@
 class HomepageController < ApplicationController
+  def index
+    @user = User.new
+
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: "users#index"
+  root to: "homepage#index"
 
   resources :users do
     member do
