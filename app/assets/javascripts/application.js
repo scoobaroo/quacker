188764838fc07e8cb3dd9e42ddb0b1fd44d2336f
scoobaroo <scoobaroo415@gmail.com>
2016-05-16 15:47:16 -0700

    half working infowindow

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,29 +21,82 @@
 //= require what-input
 //= require_tree .
 Turbolinks.enableProgressBar();
-var handler = Gmaps.build('Google');
+//
+
 $.ajax({
   method: "GET",
   url: "/tweets",
   success: getTweetsSuccess
 });
+
 function getTweetsSuccess(tweets) {
-  console.log(tweets);
-  var marker = [];
-  handler.buildMap({internal:{id:'multi_markers'}, provider: {zoom:2, center: {lat: -34, lng: 151}}}, function(){
-    for(i=0; i<tweets.length;i++){
-    marker[i] = handler.addMarker(
-      {lat:tweets[i].latitude, lng:tweets[i].longitude}
-    );
+  var myLatLng = {lat: 37.78, lng: -122.44};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom:2,
+    center: myLatLng
+  });
+
+  for (var i = 0; i < tweets.length; i++) {
+    if (tweets[i].latitude && tweets[i].longitude) {
+      var infowindow = new google.maps.InfoWindow({
+        content: "<h3><a>" + tweets[i].title + tweets[i].body+"</a></h3>"
+      });
+
+      var marker = new google.maps.Marker({
+        position: { lat: tweets[i].latitude, lng: tweets[i].longitude },
+        map: map,
+        title: tweets[i].title
+      });
+
+      marker.addListener('click', function() {
+        infowindow.open(map, marker);
+      });
     }
   }
-);
-handler.bounds.extendWith(marker);
-handler.fitMapToBounds();
 }
 
-//     { lat: 43, lng: 3.5},
-//     { lat: 45, lng: 4},
-//     { lat: 47, lng: 3.5},
-//     { lat: 49, lng: 4},
-//     { lat: 51, lng: 3.5}
+
+  // var marker = [];
+  // for(i=0; i<tweets.length;i++){
+  //   var lat=tweets[i].latitude;
+  //   console.log('rla',lat);
+  //   var long=tweets[i].longitude;
+  //   console.log('rlo',long);
+  //   console.log('map', map);
+  //   new google.maps.Marker({
+  //     position: new google.maps.LatLng(lat,long),
+  //     map: map,
+  //     title: 'Hello !'
+  //   });
+  //   }
+  // }
+
+    // function createSidebarLi(tweet){
+    //   return ("<li><a>" + tweet.title + tweet.body+"</a></li>");
+    // }
+    // function bindLiToMarker($li, marker){
+    //   $li.on('click', function(){
+    //     handler.getMap().setZoom(14);
+    //     marker.setMap(handler.getMap()); //because clusterer removes map property from marker
+    //     marker.panTo();
+    //     google.maps.event.trigger(marker.getServiceObject(), 'click');
+    //   });
+    // }
+    // function createSidebar(marker){
+    //   _.each(marker, function(json){
+    //     var $li = $( createSidebarLi(json) );
+    //     $li.appendTo('#sidebar_container');
+    //     bindLiToMarker($li, json.marker);
+    //   });
+    // }
+    // _.each(marker, function(json, index){
+    //   json.marker = marker[index];
+    // });
+    // createSidebar(marker);
+
+
+    // { lat: 43, lng: 3.5},
+    // { lat: 45, lng: 4},
+    // { lat: 47, lng: 3.5},
+    // { lat: 49, lng: 4},
+    // { lat: 51, lng: 3.5}
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -2,6 +2,19 @@ class TweetsController < ApplicationController
 
   def index
     @tweets = Tweet.all
+    # @hash = Gmaps4rails.build_markers(@tweets) do |tweet, marker|
+    #   marker.lat tweet.latitude
+    #   marker.lng tweet.longitude
+    #   marker.infowindow  "<b>#{tweet.body}#{tweet.title}</b>"
+    #   marker.picture({
+    #                 :url => "http://www.fancyicons.com/free-icons/219/ree/png/256/yellow_ducky_256.png",
+    #                 :width   => 32,
+    #                 :height  => 32
+    #                })
+    #   marker.title   "#{tweet.title}"
+    #   marker.json({ :id => tweet.id })
+    # end
+
     respond_to do |format|
       format.json { render :json=> @tweets }
       format.html {
