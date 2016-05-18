3ed73cd437ca76dadebb77e38e0a2dd0fad3416b
ilterates <iltates@gmail.com>
2016-05-17 21:04:22 -0700

    added some validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,12 @@ class User < ActiveRecord::Base
   # friendly_id :username, use: :slugged
 
   has_secure_password
-
-
   mount_uploader :avatar, AvatarUploader
+
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { in: 6..20 }
+
 
   has_many :tweets
   has_many :comments
@@ -31,7 +34,6 @@ class User < ActiveRecord::Base
       all
     end
   end
-
 
   def follow(other_user)
     active_relationships.create(followed_id: other_user.id)
